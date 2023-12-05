9e357cf7fb00d857d6c12ef62820de46d4914b9a
John Esmilio <114704051+john-esmilio@users.noreply.github.com>
2023-12-04 19:28:51 -0700

    Update exercisesData.js
    api key

--- app/exercises/exercisesData.js
+++ app/exercises/exercisesData.js
@@ -3,7 +3,7 @@
 export const fetchExerciseData = async () => {
   const response = await fetch('https://exercises-by-api-ninjas.p.rapidapi.com/v1/exercises', {
     headers: {
-      'X-RapidAPI-Key': process.env.NEXT_PUBLIC_RAPIDAPI_KEY,
+      'X-RapidAPI-Key': process.env.RAPIDAPI_KEY,
       'X-RapidAPI-Host': 'exercises-by-api-ninjas.p.rapidapi.com'
     }
   });
